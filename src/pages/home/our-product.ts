5304405ae300eeadd05230149359c8ed3546f9e1
Christoforus Surjoputro <christoforus@alterra.id>
2020-09-19 18:34:24 +0700

    Add alt attribute to pump and electric-motor image

--- src/pages/home/our-product.ts
+++ src/pages/home/our-product.ts
@@ -36,12 +36,12 @@ export class OurProduct extends LitElement {
       <h1>Produk Kami</h1>
       <div id="container">
         <div class="section-container">
-          <img src=${pump} />
+          <img src=${pump} alt="Pompa Industri" />
           <h3>Pompa Industri</h3>
           <p>Produk unggulan kami seperti pompa Franklin, Flugo, dll.</p>
         </div>
         <div class="section-container">
-          <img src=${electricMotor} />
+          <img src=${electricMotor} alt="Motor Elektrik" />
           <h3>Motor Elektrik</h3>
           <p>Produk unggulan kami seperti motor Alliance, Franklin, dll.</p>
         </div>
